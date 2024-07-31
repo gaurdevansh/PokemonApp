8641e3fbfce9c9331dfd759920a9167efff8f2f8
Devansh <devanshgaur75@gmail.com>
2024-07-31 23:10:51 +0530

    Check internet connectivity

--- app/src/main/java/com/example/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.pokemonapp
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,11 +12,11 @@ import androidx.lifecycle.lifecycleScope
 import com.example.pokemonapp.data.PokemonBaseResponse
 import com.example.pokemonapp.databinding.ActivityMainBinding
 import com.example.pokemonapp.remote.RetrofitInstance
+import com.example.pokemonapp.utils.NetworkUtil
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var pokemonList: List<PokemonBaseResponse>
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,14 +33,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        fetchPokemonData()
+        if (NetworkUtil.isInternetAvailable(this)) {
+            fetchPokemonData()
+        } else {
+            Toast.makeText(this, "Connect to Internet", Toast.LENGTH_SHORT).show()
+            binding.data.text = "No Internet Connection!"
+        }
     }
 
     private fun fetchPokemonData() {
         try {
             lifecycleScope.launch {
                 val response =  RetrofitInstance.api.getPokemonList(0, 20)
-                binding.data.text = response.toString()
+                binding.data.text = response.results.map {
+                    "$it\n"
+                }.toString()
             }
         } catch (e: Exception) {
             e.printStackTrace()
